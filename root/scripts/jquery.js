b2efd02dfaba1f3650e31ef3fce43dd55ce57b59
ChrisMJones1 <chrisjns679@gmail.com>
2019-10-31 13:45:07 -0400

    init

---
+++ root/scripts/jquery.js
@@ -0,0 +1,11 @@
+//Get all the images being used
+var image_tShirt_white = "images/tshirt-white.png";
+var image_tShirt_red = "images/tshirt-red.png";
+var image_tShirt_purple = "images/tshirt-purple.png";
+var image_tShirt_green = "images/tshirt-green.png";
+var image_tShirt_blue = "images/tshirt-blue.png";
+var image_tShirt_black = "images/tshirt-black.png";
+
+$(document).ready(function) {
+  
+};
